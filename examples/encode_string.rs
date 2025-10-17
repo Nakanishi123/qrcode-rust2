23f9be8260af36b895545b56b135579f219bf72b
Shun Sakai <sorairolake@protonmail.ch>
2025-10-18 08:07:08 +0900

    feat: Update examples

--- examples/encode_string.rs
+++ examples/encode_string.rs
@@ -3,14 +3,106 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-use qrcode2::QrCode;
+use anyhow::Context;
+use clap::{Parser, ValueEnum};
+use qrcode2::{EcLevel, QrCode, Version};
 
-fn main() {
-    let code = QrCode::new(b"Hello").unwrap();
-    let string = code
-        .render::<char>()
-        .quiet_zone(false)
-        .module_dimensions(2, 1)
-        .build();
-    println!("{string}");
+#[derive(Debug, Parser)]
+#[command(version, about)]
+struct Opt {
+    /// Error correction level.
+    #[arg(
+        short('l'),
+        long,
+        value_enum,
+        default_value_t,
+        value_name("LEVEL"),
+        ignore_case(true)
+    )]
+    error_correction_level: Ecc,
+
+    /// The version of the symbol.
+    #[arg(short('v'), long, num_args(1..=2), value_name("NUMBER"))]
+    symbol_version: Option<Vec<i16>>,
+
+    /// The type of QR code.
+    #[arg(
+        long,
+        value_enum,
+        default_value_t,
+        value_name("TYPE"),
+        ignore_case(true)
+    )]
+    variant: Variant,
+
+    /// Input data.
+    #[arg(value_name("STRING"))]
+    input: String,
+}
+
+#[derive(Clone, Debug, Default, ValueEnum)]
+enum Ecc {
+    /// Level L.
+    L,
+
+    /// Level M.
+    #[default]
+    M,
+
+    /// Level Q.
+    Q,
+
+    /// Level H.
+    H,
+}
+
+impl From<Ecc> for EcLevel {
+    fn from(level: Ecc) -> Self {
+        match level {
+            Ecc::L => Self::L,
+            Ecc::M => Self::M,
+            Ecc::Q => Self::Q,
+            Ecc::H => Self::H,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default, ValueEnum)]
+enum Variant {
+    /// Normal QR code.
+    #[default]
+    Normal,
+
+    /// Micro QR code.
+    Micro,
+
+    /// rMQR code.
+    Rmqr,
+}
+
+fn main() -> anyhow::Result<()> {
+    let opt = Opt::parse();
+
+    let input = opt.input;
+    let ec_level = opt.error_correction_level.into();
+    let code = if let Some(sv) = opt.symbol_version {
+        let version = match opt.variant {
+            Variant::Normal => Version::Normal(sv[0]),
+            Variant::Micro => Version::Micro(sv[0]),
+            Variant::Rmqr => Version::RectMicro(sv[0], sv[1]),
+        };
+        QrCode::with_version(input, version, ec_level)
+    } else {
+        match opt.variant {
+            Variant::Normal => QrCode::with_error_correction_level(input, ec_level),
+            Variant::Micro => QrCode::micro_with_error_correction_level(input, ec_level),
+            Variant::Rmqr => QrCode::rect_micro_with_error_correction_level(input, ec_level),
+        }
+    }
+    .context("could not construct a QR code")?;
+
+    let image = code.render::<char>().module_dimensions(2, 1).build();
+
+    println!("{image}");
+    Ok(())
 }
--- examples/encode_svg.rs
+++ examples/encode_svg.rs
@@ -3,15 +3,121 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+use anyhow::Context;
+use clap::{Parser, ValueEnum};
+use csscolorparser::Color;
 use qrcode2::{EcLevel, QrCode, Version, render::svg};
 
-fn main() {
-    let code = QrCode::with_version(b"01234567", Version::Micro(2), EcLevel::L).unwrap();
+#[derive(Debug, Parser)]
+#[command(version, about)]
+struct Opt {
+    /// Error correction level.
+    #[arg(
+        short('l'),
+        long,
+        value_enum,
+        default_value_t,
+        value_name("LEVEL"),
+        ignore_case(true)
+    )]
+    error_correction_level: Ecc,
+
+    /// The version of the symbol.
+    #[arg(short('v'), long, num_args(1..=2), value_name("NUMBER"))]
+    symbol_version: Option<Vec<i16>>,
+
+    /// The type of QR code.
+    #[arg(
+        long,
+        value_enum,
+        default_value_t,
+        value_name("TYPE"),
+        ignore_case(true)
+    )]
+    variant: Variant,
+
+    /// Foreground color.
+    #[arg(long, default_value("black"), value_name("COLOR"))]
+    foreground: Color,
+
+    /// Background color.
+    #[arg(long, default_value("white"), value_name("COLOR"))]
+    background: Color,
+
+    /// Input data.
+    #[arg(value_name("STRING"))]
+    input: String,
+}
+
+#[derive(Clone, Debug, Default, ValueEnum)]
+enum Ecc {
+    /// Level L.
+    L,
+
+    /// Level M.
+    #[default]
+    M,
+
+    /// Level Q.
+    Q,
+
+    /// Level H.
+    H,
+}
+
+impl From<Ecc> for EcLevel {
+    fn from(level: Ecc) -> Self {
+        match level {
+            Ecc::L => Self::L,
+            Ecc::M => Self::M,
+            Ecc::Q => Self::Q,
+            Ecc::H => Self::H,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default, ValueEnum)]
+enum Variant {
+    /// Normal QR code.
+    #[default]
+    Normal,
+
+    /// Micro QR code.
+    Micro,
+
+    /// rMQR code.
+    Rmqr,
+}
+
+fn main() -> anyhow::Result<()> {
+    let opt = Opt::parse();
+
+    let input = opt.input;
+    let ec_level = opt.error_correction_level.into();
+    let code = if let Some(sv) = opt.symbol_version {
+        let version = match opt.variant {
+            Variant::Normal => Version::Normal(sv[0]),
+            Variant::Micro => Version::Micro(sv[0]),
+            Variant::Rmqr => Version::RectMicro(sv[0], sv[1]),
+        };
+        QrCode::with_version(input, version, ec_level)
+    } else {
+        match opt.variant {
+            Variant::Normal => QrCode::with_error_correction_level(input, ec_level),
+            Variant::Micro => QrCode::micro_with_error_correction_level(input, ec_level),
+            Variant::Rmqr => QrCode::rect_micro_with_error_correction_level(input, ec_level),
+        }
+    }
+    .context("could not construct a QR code")?;
+
+    let (foreground, background) = (opt.foreground.to_css_hex(), opt.background.to_css_hex());
+    let (foreground, background) = (svg::Color(&foreground), svg::Color(&background));
     let image = code
         .render()
-        .min_dimensions(200, 200)
-        .dark_color(svg::Color("#800000"))
-        .light_color(svg::Color("#ffff80"))
+        .dark_color(foreground)
+        .light_color(background)
         .build();
+
     println!("{image}");
+    Ok(())
 }
